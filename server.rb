4acb0dc507cecdc134f2cedf91876f77cb5abb4b
Ronen Annason <ronenannason83@gmail.com>
2017-06-01 16:57:00 -0400

    Sinatra exercise 1 complete

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
 erb :index
 #instructions for what to do when user visits '/home' will go here
@@ -10,5 +14,16 @@ erb :gallery
 end
 
 get '/about_me' do
+  @skills = ['fireballs', 'Being a SuperMario', "Star Power", 'Sliding Down Flag Poles']
+  @interests = ['Princess Peach', 'Mario Bros Plumbing and Co', 'Breaking Blocks', 'Collecting Coins']
 erb :me
+end
+
+get '/favourites' do
+  @fav_links = ['www.nintendo.com', 'www.youtube.com', 'mariogames.be', 'https://en.wikipedia.org/wiki/Princess_Peach', 'https://www.giantbomb.com/koopa/3015-120/characters/']
+erb :favs
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
